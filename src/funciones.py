25d1e2ee24489c6f5c455a50d0f0f8ec43cbb0a1
crissis <pinmons123@gmail.com>
2026-03-11 12:55:41 -0500

    mejora realizada

--- src/funciones.py
+++ src/funciones.py
@@ -1,5 +1,12 @@
 def maxima_nota(notas):
     if len(notas) == 0:
         return 0
+
+    for nota in notas:
+        if nota < 0 or nota > 5:
+            raise ValueError("Las notas deben estar entre 0 y 5")
+
     return max(notas)
+
+
 
